8f307ad9360f4eff764255f77a62c452bba7d172
Daniel Frett <daniel.frett@ccci.org>
2021-07-30 13:05:35 -0400

    centralized publishing logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,11 @@ buildscript {
     }
 }
 
+plugins {
+    `maven-publish`
+    alias(libs.plugins.grgit)
+}
+
 allprojects {
     tasks.withType<JavaCompile> {
         sourceCompatibility = "1.8"
@@ -32,4 +37,24 @@ allprojects {
 tasks.withType<Wrapper> {
     gradleVersion = "7.5.1"
     distributionType = Wrapper.DistributionType.ALL
+}
+
+version = "${version}_${grgit.log { includes = listOf("HEAD") }.size}"
+allprojects {
+    group = "org.cru.mobile.fork.antlr-kotlin"
+    version = rootProject.version
+}
+
+subprojects {
+    apply(plugin = "maven-publish")
+
+    publishing {
+        repositories {
+            maven {
+                name = "cruGlobalMavenRepository"
+                setUrl("https://cruglobal.jfrog.io/artifactory/maven-cru-mobile-forks-local/")
+                credentials(PasswordCredentials::class)
+            }
+        }
+    }
 }
